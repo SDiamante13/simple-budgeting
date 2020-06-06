1fbe47db424b2b65319c8a5640263162fa4e3cff
Steven Diamante <SDiamante13@gmail.com>
2020-06-06 18:44:10 -0500

    Initial commit

---
+++ src/main/java/tech/pathtoprogramming/simplebudgeting/SimpleBudgetingApplication.java
@@ -0,0 +1,13 @@
+package tech.pathtoprogramming.simplebudgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBudgetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBudgetingApplication.class, args);
+	}
+
+}
